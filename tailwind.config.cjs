63b97b337ef22ab045fb8a471aab80de1cc2815e
BHUMIKA N U <bhumikabhumi024@gmail.com>
2025-01-30 16:49:58 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in `src/`
+    "./src/components/dashboard/HR Dashboard/**/*.{js,jsx,ts,tsx}", // HR Dashboard files
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
